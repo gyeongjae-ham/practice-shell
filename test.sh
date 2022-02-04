c49fd87b8879d12068015d5f90435f899556eec6
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-02-04 15:30:10 +0900

    TEST: 조건문 테스트

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-exec sh ./hello.sh 1 2
+request=$1
+
+if [ "${request}" == "access" ]; then
+    echo "${request} restart"
+    cd ~/Desktop/hiyee-dev/shell-practice && ./hello.sh 1 2
+elif [ "${request}" == "refuse" ]; then
+    echo "Failed"
+fi
