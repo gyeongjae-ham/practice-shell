a2b121b3e6c0fb49a2f7400bb128135bfb1c50a2
hiyee-gj <gyeongjae.h.dev@gmail.com>
2022-02-17 17:37:17 +0900

    TEST: command.sh test 실행

--- test.sh
+++ test.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-request=$1
+restartDirectory=$1
 
-if [ "${request}" == "access" ]; then
-    echo "${request} restart"
-    cd ~/Desktop/hiyee-dev/shell-practice && ./hello.sh 1 2
-elif [ "${request}" == "refuse" ]; then
-    echo "Failed"
+if [ "${restartDirectory}" == "node-webhook" ]; then
+        echo "${restartDirectory} restart"
+        sh ~/Desktop/hiyee-dev/shell-practice/command.sh
+elif [ "${restartDirectory}" == "new-email.rainbow.co.kr-email" ]; then
+        echo "${restartDirectory} restart"
+        sh ~/Desktop/hiyee-dev/shell-practice/command.sh
+elif [ "${restartDirectory}" == "rainbow.co.kr-www-new" ]; then
+        echo "${restartDirectory} restart"
+        sh ~/Desktop/hiyee-dev/shell-practice/command.sh
 fi
+
+
+#request=$1
+#
+#if [ "${request}" == "access" ]; then
+#    echo "${request} restart"
+#    cd ~/Desktop/hiyee-dev/shell-practice && ./hello.sh 1 2
+#elif [ "${request}" == "refuse" ]; then
+#    echo "Failed"
+#fi
